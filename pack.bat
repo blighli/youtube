7d096e3bc3d078bd4c5a667a48f72e951f635e5c
liqilei <blighli@gmail.com>
2025-11-03 02:34:56 +0800

    improve pack.bat

--- pack.bat
+++ pack.bat
@@ -1,2 +1,14 @@
-pyinstaller -i youtube.ico -wF main.py
-dist\main.exe
+:设定打包文件名称
+set name=main
+
+:执行打包命令
+pyinstaller -i youtube.ico -wF %name%.py
+
+:复制exe到当前目录
+copy .\dist\%name%.exe .
+
+:删除过程文件及文件夹
+rmdir /s /q __pycache__
+rmdir /s /q build
+rmdir /s /q dist
+del /q %name%.spec
