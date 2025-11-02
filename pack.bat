8fd9b5a35bf2e86608d587b361f4c2479c49bdd0
liqilei <blighli@gmail.com>
2025-11-03 02:38:31 +0800

    fix pack.bat to execute with exeName

--- pack.bat
+++ pack.bat
@@ -1,14 +1,17 @@
 :设定打包文件名称
-set name=main
+set srcName=main
+set exeName=YoutubeGUI
 
 :执行打包命令
-pyinstaller -i youtube.ico -wF %name%.py
+pyinstaller -i youtube.ico -wF %srcName%.py
 
 :复制exe到当前目录
-copy .\dist\%name%.exe .
+copy .\dist\%srcName%.exe .\%exeName%.exe /Y
 
 :删除过程文件及文件夹
 rmdir /s /q __pycache__
 rmdir /s /q build
 rmdir /s /q dist
-del /q %name%.spec
+del /q %srcName%.spec
+
+.\%exeName%.exe
